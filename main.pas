38f139a14ad3f739036704f3a79dc8329a549bcf
Kostya <kuzko6d@yandex.ru>
2024-11-03 10:54:46 +0300

    Add operations functions. Stable.

--- main.pas
+++ main.pas
@@ -8,10 +8,118 @@ var
   // main block
   i: integer;
   result: double = 0;
+  argument: double;
+  res_sign: boolean = true;
+  arg_sign: boolean;
 // init block
   accuracy: double;
   out_base: start_args;
 
+    // SUB FUNCTIONS //
+// check overflow and compute adding
+function subAdding(res, arg: double): double;
+begin
+  if (res + arg >= 5.0 * Power(10, -324)) and (res + arg <= 1.7 * Power(10, 308)) then
+    subAdding := res + arg
+end;
+
+// check overflow and compute adding
+function subSubtraction(res, arg: double): double;
+begin
+  if (res - arg >= 5.0 * Power(10, -324)) and (res - arg <= 1.7 * Power(10, 308)) then
+    subSubtraction := res - arg
+end;
+
+// check overflow and compute multiplication
+function subMultiplicate(res, arg: double): double;
+begin
+  // check overflow
+  if (res * arg >= 5.0 * Power(10, -324)) and (res * arg <= 1.7 * Power(10, 308)) then
+    subMultiplicate := res * arg
+  else
+  begin
+    writeln('Result overflow.');
+    writeln('(result out of double range)');
+    halt(1);
+  end;
+end;
+
+// check overflow and compute division
+function subDivision(res, arg: double): double;
+begin
+  // catch division by zero
+  if arg = 0 then
+  begin
+    writeln('Division by zero.');
+    writeln('(can"t divide by zero)');
+    halt(1);
+  end
+      // check overflow
+  else if (res / arg >= 5.0 * Power(10, -324)) and (res / arg <= 1.7 * Power(10, 308)) then
+    subDivision := res / arg
+  else
+  begin
+    writeln('Result overflow.');
+    writeln('(result out of double range)');
+    halt(1);
+  end;
+end;
+
+    //Uses in main\\
+// adding procedure
+procedure adding(var res: double; arg: double; var res_sign: boolean; arg_sign: boolean);
+begin
+  // ++ / -- -> sum and don't change sign
+  if (res_sign and arg_sign) or (not(res_sign) and not(arg_sign)) then
+    res := subAdding(res, arg)
+  else
+  begin
+    // first > second -> first - sec
+    if res > arg then
+    begin
+      res := subSubtraction(res, arg);
+      // if result < 0
+      if res_sign < arg_sign then
+        res_sign := false;
+    end
+        // first < second -> sec - first
+    else
+    begin
+      res := subSubtraction(arg, res);
+      if res_sign > arg_sign then
+        res_sign := false;
+    end;
+  end;
+end;
+
+// multiplicate procedure
+procedure multiplicate(var res: double; arg: double; var res_sign: boolean; arg_sign: boolean);
+begin
+  // ++, -- -> +
+  if (res_sign and arg_sign) or (not(res_sign) and not(arg_sign)) then
+    res_sign := true
+      // +-, -+ -> -
+  else
+    res_sign := false;
+  // write result
+  res := subMultiplicate(res, arg);
+end;
+
+// division procedure
+procedure division(var res: double; arg: double; var res_sign: boolean; arg_sign: boolean);
+begin
+  // ++, -- -> +
+  if (res_sign and arg_sign) or (not(res_sign) and not(arg_sign)) then
+    res_sign := true
+      // +-, -+ -> -
+  else
+    res_sign := false;
+  // write result
+  res := subDivision(res, arg);
+end;
+
+
+    // MAIN FUNCTIONS //
 // init procedure. read start arguments
 procedure mainReadInit(var accuracy: double; var out_base: start_args);
 var
@@ -59,6 +167,4 @@ begin
   for i:=2 to length(out_base) do
     writeln(i - 1, ' answ base: ', out_base[i]);
 
-  // read line
-  {mainReadInputLine(result);}
 end.
