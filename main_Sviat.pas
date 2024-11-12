b8806f978cf727b8dd1722b2b9175b40949a6943
CveticSemecvetic <Salarin2006@gmail.com>
2024-11-12 16:55:01 +0300

    Update main_Sviat.pas

--- main_Sviat.pas
+++ main_Sviat.pas
@@ -1,85 +1,100 @@
-program main;
-uses sysutils, math;
+program big_problem;
+uses crt, sysutils, math;
 
 type
   start_args = packed array of integer;
 
 var
-  (*  main block *)
-  i: integer;
+  {main block}
   res_sign: boolean = true;
   result: double = 0;
   arg_operation: char;
   arg_sign: boolean;
   argument: double;
   fin: boolean = false;
-(*  init block *)
+  {block of initialization}
   accuracy: double;
   out_base: start_args;
 
-    (*  SUB FUNCTIONS // *)
-(*  check overflow and compute adding *)
+    {Dop functions}
+{functions use main finish inside}
+procedure mainFinish(res_sign: boolean; result: double; accuracy: double; var out_base: start_args); forward;
+
+{checking for overflow and do adding}
 function subAdding(res, arg: double): double;
 begin
-  if (res >= minDouble + arg) and (res <= maxDouble - arg) then
+  if (res <= maxDouble - arg) then
     subAdding := res + arg
   else
-  begin
-    writeln('Result overflow.');
-    writeln('(result out of double range. adding)');
-    halt(1);
-  end;
+    begin
+      writeln('Result overflow.');
+      writeln('(result out of double range. adding)');
+      halt(1);
+    end;
 end;
 
-(*  check overflow and compute substraction *)
+{do substraction}
 function subSubtraction(res, arg: double): double;
 begin
-  if (res >= minDouble + arg) and (res >= maxDouble + arg) then
-    subSubtraction := res - arg
-  else
-  begin
-    writeln('Result overflow.');
-    writeln('(result out of double range. substraction)');
-    halt(1);
-  end;
+  subSubtraction := res - arg
 end;
 
-(*  check overflow and compute multiplication *)
+{checking for overflow and do multiplication}
 function subMultiplicate(res, arg: double): double;
 begin
-  (*  check overflow *)
-  if (res >= minDouble / arg) and (res <= maxDouble / arg) then
+  {if arg = 0 then chtcking for overflow is incorrect}
+  if (arg=0) then
+    begin
+      res:=0;
+      res_sign:=true;
+    end
+  {checking for overflow}
+  else if ((res >= minDouble / arg) and (res <= maxDouble / arg)) then
     subMultiplicate := res * arg
   else
-  begin
-    writeln('Result overflow.');
-    writeln('(result out of double range. multiplicate)');
-    halt(1);
-  end;
+    begin
+      writeln('Result overflow.');
+      writeln('(result out of double range. multiplicate)');
+      halt(1);
+    end;
 end;
 
-(*  check overflow and compute division *)
+{checking for overflow and do division}
 function subDivision(res, arg: double): double;
 begin
-  (*  catch division by zero *)
-  if arg = 0 then
-  begin
-    writeln('Division by zero.');
-    writeln('(can"t divide by zero)');
-    halt(1);
-  end
-      (*  check overflow *)
-  else if (res >= minDouble * arg) and (res <= maxDouble * arg) then
+  {if arg = 0 then math problem}
+  if (arg = 0) then
+    begin
+      writeln('Division by zero.');
+      halt(1);
+    end
+      {checking for overflow}
+  else if ((res >= minDouble * arg) and (res <= maxDouble * arg)) then
     subDivision := res / arg
   else
-  begin
-    writeln('Result overflow.');
-    writeln('(result out of double range. division)');
-    halt(1);
-  end;
+    begin
+      writeln('Result overflow.');
+      writeln('(result out of double range. division)');
+      halt(1);
+    end;
 end;
 
-{conversion of a number into a number system with a base of 16} 			{Для Кости и Гриши, стереть это!!!!!  Эта функция переводит числа из следующей функции в формат [0..9][a..f]}
+{checking for 16-system}
+function checking_16(c: char): boolean;
+begin
+  checking_16 := false;
+  if (((ord(c) >= ord('0')) and (ord(c) <= ord('9'))) or ((ord(c) >= ord('a')) and (ord(c) <= ord('f')))) then
+    checking_16 := true;
+end;
+
+procedure mistake_finish(result: double; accuracy: double; var arr: start_args);
+begin
+  writeln('The program terminated due to an input error, the last result received:');
+  mainFinish(res_sign, result, accuracy, arr);
+  halt(0);
+end;
+
+{conversion of a number into a number system with a base of 16}
 procedure to_16_system(res: integer; flag: boolean);
 var
   new_res: integer;
@@ -109,11 +124,10 @@ begin
     end;
 end;
 
-{conversion of trunc(res) into a number system with a base of 2..256} 		{Для Кости и Гриши, стереть это!!!!!!!!  Эта функция берет целую часть от результата и сначала переводит в кастомную систему
- счисления, а потом уже эти числа выводит в представлении [0..9][a..f]}
-procedure to_system(base, res: integer);
+{conversion of trunc(res) into a number system with a base of 2..256}
+procedure to_system(base: integer; res: longword);
 var
-  new_res: integer;
+  new_res: longword;
 
 begin
   if ((res div base) = 0) then
@@ -127,10 +141,10 @@ begin
       to_system(base, new_res);
       to_16_system((res mod base), false);
       write(' ');
-    end; 
+    end;
 end;
 
-{this function determines how many decimal places to display}			{Для Кости и Гриши, стереть это!!!!!!!!! Тут гениальная схема Сальникова по тому сколько знаков нужно}
+{this function determines how many decimal places to display}
 function after_dot_num_func(base, count: integer; accuracy, after_dot_res: double): integer;
 var
   temp_accuracy, temp_num, new_num, new_acc: double;
@@ -154,7 +168,7 @@ begin
   end;
 end;
 
-{conversion of a fractional part into a number system with a base 2..256} 	{Для Кости и Гриши, стереть это!!!!!!!! Переводим число после запятой в кастомную систему, а потом в [0..9][a..f] представление}
+{conversion of a fractional part into a number system with a base 2..256}
 procedure after_dot_to_system(base: integer; accuracy, after_dot_res: double);
 var
   after_dot_num, i: integer;
@@ -177,33 +191,33 @@ begin
 end;
 
 
-    (* Uses in main\\ *)
-(*  adding procedure *)
+{in-main functions}
+{adding procedure}
 procedure mainAdding(var res: double; arg: double; var res_sign: boolean; arg_sign: boolean);
 begin
-  (*  ++ / -- -> sum and don't change sign *)
-  if (res_sign and arg_sign) or (not(res_sign) and not(arg_sign)) then
+  {sum dont change sign}
+  if ((res_sign and arg_sign) or (not(res_sign) and not(arg_sign))) then
     res := subAdding(res, arg)
   else
-  begin
-    (*  first > second -> first - sec *)
-    if res > arg then
     begin
-      res := subSubtraction(res, arg);
-      writeln('LOG: first - sec. sucsess.');
-      (*  if result < 0 *)
-      if res_sign < arg_sign then
-        res_sign := false;
-    end
-        (*  first < second -> sec - first *)
-    else
-    begin
-      res := subSubtraction(arg, res);
-      writeln('LOG: arg - res. sucsess.');
-      if res_sign > arg_sign then
-        res_sign := false;
+      {if first > second -> first - sec}
+      if res > arg then
+        begin
+          res := subSubtraction(res, arg);
+          writeln('LOG: first - sec. sucsess.');
+          {if result < 0}
+          if res_sign < arg_sign then
+            res_sign := false;
+        end
+        {if first < second -> sec - first}
+      else
+        begin
+          res := subSubtraction(arg, res);
+          writeln('LOG: arg - res. sucsess.');
+          if res_sign > arg_sign then
+            res_sign := false;
+        end;
     end;
-  end;
 end;
 
 (*  multiplicate procedure *)
@@ -272,77 +286,337 @@ begin
       out_base[i] := tempInt;
 end;
 
-(*  read input procedure. read operation and num and handle finish command *)
-procedure mainReadInput(var arg_operation: char; var arg_sign: boolean; var argument: double; var fin: boolean);
+{comment for the team, a working procedure for processing input parameters, processing all exceptional operations with spaces, finish, comments inside the input data and almost accepted by Salnikov}
+procedure mainReadInput(var operation: char; var znak: boolean; var chislo: double; var fin: boolean);
+var
+  base, i, fin_fl, num: integer;
+  fl_operation, fl_znak, fl_base, fl_dot, fl_comment: boolean;
+  c, d: char;
+  operation_str: string;
 begin
+  operation_str := '+-*/';
+  fl_operation := false;
+  fl_dot := true;
+  fl_znak := true;
+  fl_base := true;
+  fl_comment := false;
+  chislo := 0;
+  znak := true;
+  fin_fl := 0;
+  fin := false;
+  base := 0;
+  repeat
+    num := 0;
+    read(c);
+
+      {checking the line for comments}
+    if ord(c) = ord('#') then
+    begin
+      fl_comment := true;
+      continue;
+    end;
+
+      {the condition for skipping all spaces and tabs, or if its comment}
+    if ((ord(c) = ord(' ')) or (ord(c) = 9) or (fl_comment)) then
+      continue;
+
+      {entering the operation sign or checking the first significant character of the string at the beginning of the word finish}
+    if not(fl_operation) then
+    begin
+      if (pos(c, operation_str) <> 0) then
+      begin
+        fl_operation := true;
+        fl_base := false;
+        operation := c;
+        continue;
+      end
+      else
+      begin
+        if ord(c) = ord('f') then
+        begin
+          fin_fl := 1;
+          continue;
+        end
+        else
+          if ((ord(c) = ord('i')) and (fin_fl = 1)) then
+          begin
+            fin_fl := 2;
+            continue;
+          end
+          else
+            if ((ord(c) = ord('n')) and (fin_fl = 2)) then
+            begin
+              fin_fl := 3;
+              continue;
+            end
+            else
+              if ((ord(c) = ord('i')) and (fin_fl = 3)) then
+              begin
+                fin_fl := 4;
+                continue;
+              end
+              else
+                if ((ord(c) = ord('s')) and (fin_fl = 4)) then
+                begin
+                  fin_fl := 5;
+                  continue;
+                end
+                else
+                  if ((ord(c) = ord('h')) and (fin_fl = 5)) then
+                  begin
+                    fin := true;
+                    continue;
+                  end
+                  else
+                    mistake_finish(result, accuracy, out_base);
+      end;
+    end;
+
+      {entering the number system}
+    if not(fl_base) then
+    begin
+      if ((ord(c) >= ord('0')) and (ord(c) <= ord('9'))) then
+      begin
+        while (ord(c) <> ord(':')) do
+        begin
+          base := base * 10 + (ord(c) - ord('0'));
+          read(c);
+          if ((ord(c) >= ord('0')) and (ord(c) <= ord('9'))) then
+            continue
+          else
+            break;
+        end;
+      end;
+      if ((base > 256) or (base < 2)) then
+        mistake_finish(result, accuracy, out_base);
+      if (ord(c) = ord(':')) and (base <> 0) then
+      begin
+        fl_base := true;
+        fl_znak := false;
+        read(d);
+        if (ord(d) <> ord(' ')) then
+          mistake_finish(result, accuracy, out_base);
+        continue;
+      end
+      else
+        mistake_finish(result, accuracy, out_base);
+    end;
+
+
+      {entering a number sign}
+    if not(fl_znak) then //input of sign
+      case c of
+        '+':
+        begin
+          znak := true;
+          fl_znak := true;
+          fl_dot := false;
+          continue;
+        end;
+        '-':
+        begin
+          znak := false;
+          fl_znak := true;
+          fl_dot := false;
+          continue;
+        end
+      else
+        if checking_16(c) then
+        begin
+          znak := true;
+          fl_znak := true;
+          fl_dot := false;
+        end
+        else
+          mistake_finish(result, accuracy, out_base);
+      end;
+
+      {entering an integer part of a number}
+    i := 0;
+
+    if not(fl_dot) then
+    begin
+      if (checking_16(c)) then
+      begin
+        while (ord(c) <> ord('.')) do
+        begin
+          i := i + 1;
+          read(d);
+          if (checking_16(c) and checking_16(d)) then
+          begin
+            if ((ord(c) >= ord('0')) and (ord(c) <= ord('9'))) then
+              num := num + 16 * (ord(c) - ord('0'))
+            else
+              num := num + 16 * (10 + ord(c) - ord('a'));
+            if ((ord(d) >= ord('0')) and (ord(d) <= ord('9'))) then
+              num := num + (ord(d) - ord('0'))
+            else
+              num := num + (10 +ord(d) - ord('a'));
+          end
+          else
+            mistake_finish(result, accuracy, out_base);
+
+          if (num >= base) then
+            mistake_finish(result, accuracy, out_base)
+          else
+          begin
+            chislo := chislo * base + num;
+          end;
+          num := 0;
+          read(c);
+          if (ord(c) = ord(' ')) then
+          begin
+            read(c);
+            continue;
+          end;
+          if (checking_16(c)) then
+            continue
+          else
+            break;
+        end;
+      end;
+      if ((ord(c) = ord('.')) and (i <> 0)) then
+      begin
+        fl_dot := true;
+        i := 0;
+        continue;
+      end
+      else
+        mistake_finish(result, accuracy, out_base);
+    end;
+
+      {entering the fractional part of a number}
+    if fl_dot and fl_operation and fl_znak and fl_base then
+    begin
+      if (checking_16(c)) then
+      begin
+        i := 1;
+        while (ord(c) <> 10) do
+        begin
+          read(d);
+          if (checking_16(c) and checking_16(d)) then
+          begin
+            if ((ord(c) >= ord('0')) and (ord(c) <= ord('9'))) then
+              num := num + 16 * (ord(c) - ord('0'))
+            else
+              num := num + 16 * (10 + ord(c) - ord('a'));
+            if ((ord(d) >= ord('0')) and (ord(d) <= ord('9'))) then
+              num := num + (ord(d) - ord('0'))
+            else
+              num := num + 10 + (ord(d) - ord('a'));
+          end
+          else
+            mistake_finish(result, accuracy, out_base);
+
+          if (num >= base) then
+            mistake_finish(result, accuracy, out_base)
+          else
+          begin
+            chislo := chislo + num / exp(i * LN(base));
+          end;
+          num := 0;
+          i := i + 1;
+          read(c);
+          if (ord(c) = ord(' ')) then
+          begin
+            while (ord(c) = ord(' ')) do
+            begin
+              read(c);
+            end;
+          end;
+          if (ord(c) = ord('#')) then
+          begin
+            fl_comment := true;
+            break;
+          end;
+          if (checking_16(c)) then
+            continue
+          else
+            break;
+        end;
+      end;
+      if (fl_comment = true) then
+        continue;
+      if ((ord(c) = 10) and (i <> 0)) then
+      begin
+        i := 0;
+        continue;
+      end
+      else
+      begin
+        writeln('here');
+        mistake_finish(result, accuracy, out_base);
+      end;
+    end;
+  until (ord(c) = 10) or (fin = true);
+
 end;
 
-(*  finish procedure. finish program and writre result in all init bases *)
+{finish procedure. finish program and write result in all init bases}
 procedure mainFinish(res_sign: boolean; result: double; accuracy: double; var out_base: start_args);
 var
   after_dot_res: double;
-  before_dot_res, i: integer
+  i, before_dot_res: longword; 
 
 begin
 
-  after_dot_res:=(result-(trunc(result)));
-  before_dot_res:=trunc(result);
-  
+  after_dot_res := (result - (trunc(result)));
+  before_dot_res := trunc(result);
+
   {output with formatting}
   for i:=2 to ParamCount do
+  begin
+    if (out_base[i] <= 9) then
     begin
-      if (out_base[i]<=9) then
-        begin
-          write(out_base[i], '     ');
-          if (res_sign = false) then
-            write('-');
-          to_system(out_base[i], before_dot_res);
-          write(' . ');
-          after_dot_to_system(out_base[i], accuracy, after_dot_res);
-          writeln;
-        end;
-      if ((out_base[i]>=10) and (out_base[i]<=99)) then
-        begin
-          write(out_base[i], '    ');
-          if (res_sign = false) then
-            write('-'); 
-          to_system(out_base[i], before_dot_res);
-          write(' . ');
-          after_dot_to_system(out_base[i], accuracy, after_dot_res);
-          writeln;
-        end;
-      if (out_base[i]>99) then
-        begin
-          write(out_base[i], '   ');
-          if (res_sign = false) then
-            write('-');
-          to_system(out_base[i], before_dot_res);
-          write(' . ');
-          after_dot_to_system(out_base[i], accuracy, after_dot_res);
-        end;
+      write(out_base[i], '     ');
+      if (res_sign = false) then
+        write('-');
+      to_system(out_base[i], before_dot_res);
+      write(' . ');
+      after_dot_to_system(out_base[i], accuracy, after_dot_res);
+      writeln;
     end;
+    if ((out_base[i] >= 10) and (out_base[i] <= 99)) then
+    begin
+      write(out_base[i], '    ');
+      if (res_sign = false) then
+        write('-');
+      to_system(out_base[i], before_dot_res);
+      write(' . ');
+      after_dot_to_system(out_base[i], accuracy, after_dot_res);
+      writeln;
+    end;
+    if (out_base[i] > 99) then
+    begin
+      write(out_base[i], '   ');
+      if (res_sign = false) then
+        write('-');
+      to_system(out_base[i], before_dot_res);
+      write(' . ');
+      after_dot_to_system(out_base[i], accuracy, after_dot_res);
+      writeln;
+    end;
+  end;
 
 end;
 
 
 begin
-  (*  initialize *)
+  {initialization}
   mainReadInit(accuracy, out_base);
 
-  (*  write initialize results *)
-  writeln('the accuracy is: ', accuracy: 0: 5);
-  for i:=2 to length(out_base) do
-    writeln(i - 1, ' answ base: ', out_base[i]);
-
-  (*  main cycle *)
+  {main cycle}
   while true do
   begin
-    (*  read input *)
+    {read input}
     mainReadInput(arg_operation, arg_sign, argument, fin);
-    (*  if finish command detected *)
+    {if finish detected}
     if fin then
-      mainFinish(result, accuracy, out_base)
-        (*  process operation *)
+      begin
+        mainFinish(res_sign, result, accuracy, out_base);
+        halt(0);
+      end
+    {process operation}
     else
       case arg_operation of
         '+': mainAdding(result, argument, res_sign, arg_sign);
@@ -350,15 +624,14 @@ begin
         '/': mainDivision(result, argument, res_sign, arg_sign);
         '-':
         begin
-          (*  argumen sign * (-1) *)
-          if arg_sign = false then
+          {argument sign * (-1)}
+          if (arg_sign = false) then
             arg_sign := true
           else
             arg_sign := false;
-          (*  simple adding *)
+          {adding}
           mainAdding(result, argument, res_sign, arg_sign);
         end;
       end;
   end;
-
 end.
